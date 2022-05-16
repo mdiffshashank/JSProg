fca78bd68b70fc6f21d5c6329d5ed77cfb1275ac
Shashank Trivedi <51978440+mdiffshashank@users.noreply.github.com>
2022-05-16 21:52:57 +0530

    Add files via upload

---
+++ fibo.js
@@ -0,0 +1,19 @@
+//get nth node of the fibonacchi series
+// basic recursion program
+// function fibo(n) {
+//   if (n === 1 || n===2) return 1;
+//   return fibo(n-1)+fibo(n-2);
+// }
+
+//this will not work due to exponential time
+// console.log(fibo(50));
+
+//when memoization used , use memo as memoization object
+
+function fibo(n,memo={}){
+    if(n===1 || n===2) return 1;
+    if(n in memo) return memo[n]; //check if it is present in memo object
+    memo[n] = fibo(n-1,memo) + fibo(n-2,memo); //set the momization object value
+    return memo[n];
+}
+console.log(fibo(50));
